4dcf8030ae0c2f5aba26de0feaadff9c8513310d
ruslan4432013 <ruslan4432013@gmail.com>
2021-12-04 17:15:31 +0300

    add products from db

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -122,6 +122,9 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
